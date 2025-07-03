57f79f8bbc7acdb7cd90ed7a22f4024fe54276fd
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-07-03 11:38:08 +0530

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <Navbar></Navbar>
-    </>
-  );
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
+  return <>{!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}</>;
 }
 
 export default App;
