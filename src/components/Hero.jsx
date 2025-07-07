9d362306ae15184833bc58251bce489df0cdecc4
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-07-07 11:56:43 +0530

    hero search bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,47 @@
-import React from "react";
-import { assets } from "../assets/assets";
+import React, { useState } from "react";
+import { assets, cityList } from "../assets/assets";
 
 const Hero = () => {
+  const [pickupLocation, setpickupLocation] = useState("");
+
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center">
       <h1 className="text-4xl md:text-5xl font-semibold">
         Lukury cars on Rent
       </h1>
 
-      <form></form>
+      <form className="flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]">
+        <div className="flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8">
+          <div className="flex flex-col items-start gap-2">
+            <select
+              required
+              value={pickupLocation}
+              onChange={(e) => setpickupLocation(e.target.value)}
+            >
+              <option value="">Pickup Location</option>
+              {cityList.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
+            <p className="px-1 text-sm text-gray-500">
+              {pickupLocation ? pickupLocation : "Please select location"}
+            </p>
+          </div>
+
+          <div className="flex flex-col items-start gap-2">
+            <label htmlFor="pickup-date">Pickup Date</label>
+            <input
+              type="date"
+              id="pickup-date"
+              min={new Date().toISOString().split("T")[0]}
+              className="text-sm text-gray-500"
+              required
+            ></input>
+          </div>
+        </div>
+      </form>
       <img src={assets.main_car} alt="Car" className="max-h-74" />
     </div>
   );
